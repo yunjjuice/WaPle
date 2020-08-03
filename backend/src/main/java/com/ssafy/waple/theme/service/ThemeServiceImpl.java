ef0682cbeb5ed2e4cedb089b0fcb061dbc2f3400
kickrisk <dsghdh123@naver.com>
2020-08-03 17:52:56 +0900

    [feature] S03P13A204-45 : AOP 설정
    1. PermissionCheck (예외 던지는거 추가)
    2. WapleAspect (AOP 설정파일)
    3. AccessDeniedException
    4. ThemeController (파라메타 변경)
    5. ThemeService (파라메터 변경)
    6. ThemeServiceImpl (AOP로 인한 에러처리 로직 삭제)
    7. theme_query (쿼리문 수정)

--- backend/src/main/java/com/ssafy/waple/theme/service/ThemeServiceImpl.java
+++ backend/src/main/java/com/ssafy/waple/theme/service/ThemeServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssafy.waple.theme.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.waple.group.dto.GroupDto;
@@ -17,61 +18,31 @@ public class ThemeServiceImpl implements ThemeService {
 	@Autowired
 	ThemeDao dao;
 
-	@Autowired
-	GroupService service;
-
-	// 테마 갯수 조회
-	@Override
-	public int readLastId(int groupId) {
-		return dao.readLastId(groupId);
-	}
+	// DB 연결 안될시 나는 에러명 Could not connect to address (나중에 AOP에서 처리)
 
 	// 테마 생성
 	@Override
-	public boolean create(long userId, ThemeDto theme) {
+	public void create(String token, int groupId, ThemeDto theme) {
 
-		if (!checkUser(userId, theme.getGroupId()))
-			throw new UserNotInGroupException(userId);
-
-		int result = 0;
 		// AOP를 이용해서 그룹 아이디가 존재하는것인지 체크 할 필요가 있을듯!
-
-		// 해당 그룹의 마지막 테마번호 확인
-		int lastId = this.readLastId(theme.getGroupId());
-
-		// 테마 아이디 1 추가
-		theme.setThemeId(lastId + 1);
 
 		try {
 			// 1. 테마 이름 정규식을 통해서 유효성 검사
 			// 2. 테마 아이콘 역시 유효성 검사 필요
-			result = dao.create(theme);
-		} catch (Exception e) {
-			result = -1;
+			dao.create(theme);
+		} catch (DataAccessException e) {
 			e.printStackTrace();
 		}
-
-		return result > 0;
 	}
 
 	@Override
-	public List<ThemeDto> read(long userId, int groupId) {
-
-		if (!checkUser(userId, groupId))
-			throw new UserNotInGroupException(userId);
+	public List<ThemeDto> read(String token, int groupId) {
 
 		return dao.read(groupId);
 	}
 
 	@Override
-	public void update(long userId, int groupId, int themeId, ThemeDto theme) {
-
-		if (!checkUser(userId, groupId))
-			throw new UserNotInGroupException(userId);
-
-		// 실수 방지
-		theme.setGroupId(groupId);
-		theme.setThemeId(themeId);
+	public void update(String token, int groupId, int themeId, ThemeDto theme) {
 
 		int result = dao.update(theme);
 
@@ -82,29 +53,12 @@ public class ThemeServiceImpl implements ThemeService {
 	}
 
 	@Override
-	public void delete(long userId, int groupId, int themeId) {
-
-		if (!checkUser(userId, groupId))
-			throw new UserNotInGroupException(userId);
+	public void delete(String token, int groupId, int themeId) {
 
 		int result = dao.delete(groupId, themeId);
 
 		if (result < 1) {
 			throw new ThemeNotFoundException(groupId, themeId);
 		}
-	}
-
-	private boolean checkUser(long userId, int groupId) {
-		// 해당 그룹이 해당 유저가 속해있는지 유효성 체크하기
-		List<GroupDto> list = service.readAll(userId);
-
-		boolean isBelong = false;
-		for (GroupDto dto : list) {
-			if (dto.getGroupId() == groupId) {
-				isBelong = true;
-				break;
-			}
-		}
-		return isBelong;
 	}
 }
