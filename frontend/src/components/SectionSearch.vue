67204a875cf52a1ce3d1c9ed998c5c6253b8d7c3
최윤주 <yeomju311@naver.com>
2020-08-03 21:13:03 +0900

    [feature] S03P13A204-26 : 북마크 등록
    1. 북마크 등록 모달 창 생성
    2. 북마크 등록
    3. 입력값 제대로 넣었는지 검사
    4. 장소 검색 후 검색창에 검색어 초기화

--- frontend/src/components/SectionSearch.vue
+++ frontend/src/components/SectionSearch.vue
@@ -19,15 +19,13 @@
             <v-card-title
               class="headline"
               v-text="item.place_name"
-            >
-            </v-card-title>
+            ></v-card-title>
+            <v-card-text>
+              {{ item.road_address_name }}
+            </v-card-text>
             <v-card-actions>
-              <!-- TODO 북마크 설정 여부에 따라 아이콘 모양 다르게 하기 -->
-              <v-btn icon>
+              <v-btn icon @click.stop="showDialog(item)">
                 <v-icon>mdi-bookmark-plus-outline</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
               </v-btn>
             </v-card-actions>
           </div>
@@ -35,15 +33,64 @@
       </v-card>
     </v-col>
   </v-row>
+  <v-dialog
+    v-model="dialog"
+    width="400"
+    height="300"
+  >
+    <v-card align="center">
+      <v-card-title class="headline yellow lighten-3">북마크등록</v-card-title>
+      <v-form ref="form" v-model="valid">
+      <v-row justify="center">
+        <v-col cols="15" sm="6">
+          <v-select
+            :items="groups"
+            v-model="group"
+            item-text="name"
+            label="group"
+            return-object
+            required
+            :rules="[v => !!v || 'group is required']"
+            @change="findTheme"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col class="d-flex" cols="15" sm="6">
+          <v-select
+            :items="themes"
+            v-model="theme"
+            item-text="name"
+            label="theme"
+            return-object
+            required
+            :rules="[v => !!v || 'theme is required']"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-btn depressed color="primary" @click="isValid">추가하기</v-btn>
+      </v-row>
+      </v-form>
+    </v-card>
+  </v-dialog>
   </v-main>
 </template>
 
 <script>
 import store from '@/store/index';
+import api from '@/utils/api';
 
 export default {
   data() {
     return {
+      dialog: false,
+      place: {}, // 선택된 장소 정보
+      groups: [], // 데이터에서 받아온 그룹 목록
+      group: {}, // 선택된 그룹
+      themes: [], // 데이터에서 받아온 테마 목록
+      theme: {}, // 선택된 테마
+      valid: true,
     };
   },
   computed: {
@@ -54,6 +101,56 @@ export default {
     moveBack() {
       this.$router.go(-1);
     },
+    findTheme() {
+      api.get(`/themes/${this.group.groupId}`, { headers: { token: this.$session.get('token') } })
+        .then(({ data }) => {
+          this.themes = data;
+        });
+    },
+    showDialog(item) {
+      this.dialog = true;
+      this.place = item;
+      this.themes = [];
+      this.group = null;
+      this.theme = null;
+      this.$refs.form.resetValidation();
+    },
+    makeBookmark() {
+      // 장소 추가
+      api.post('/places', {
+        address: this.place.road_address_name,
+        lat: this.place.y,
+        lng: this.place.x,
+        name: this.place.place_name,
+        placeId: this.place.id,
+        url: this.place.place_url,
+      });
+      // .then(({ res }) => {
+      // console.log(`status code : ${res.status}`);
+      // });
+      // 북마크 등록
+      api.post('/bookmarks', {
+        groupId: this.group.groupId,
+        placeId: this.place.id,
+        themeId: this.theme.themeId,
+        userId: this.$session.get('uid'),
+      }).then(({ data }) => {
+        console.log(data);
+      });
+      this.dialog = false;
+    },
+    isValid() { // 그룹과 테마가 다 선택되었는지 확인
+      this.$refs.form.validate();
+      if (this.valid) {
+        this.makeBookmark();
+      }
+    },
+  },
+  created() {
+    api.get(`/groups/${this.$session.get('uid')}`)
+      .then(({ data }) => {
+        this.groups = data;
+      });
   },
 };
 </script>
