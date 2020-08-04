ca2a939dd99a537c97eb1bae7531064659843985
Hyejin Park <triaina0@gmail.com>
2020-08-04 17:10:51 +0900

    [update] S03P13A204-49 : Promise Exception 처리
    - create: Group Id가 없을 때, 약속 일시가 current Time보다 이전일 때, 약속 이름이 없을 때 Exception 발생
    - update : Group Id & Promise Id 없을 때, 약속 일시가 current Time보다 이전일 때, 약속 이름이 없을 때 Exception 발생
    - delete : Group Id & Promise Id 없을 때 Exception 발생

--- backend/src/main/java/com/ssafy/waple/promise/service/PromiseServiceImpl.java
+++ backend/src/main/java/com/ssafy/waple/promise/service/PromiseServiceImpl.java
@@ -3,20 +3,37 @@ package com.ssafy.waple.promise.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.waple.error.exception.InvalidValueException;
+import com.ssafy.waple.group.exception.GroupNotFoundException;
 import com.ssafy.waple.promise.dao.PromiseDao;
 import com.ssafy.waple.promise.dto.PromiseDto;
+import com.ssafy.waple.promise.exception.PromiseNotFoundException;
 
 @Service
 public class PromiseServiceImpl implements PromiseService {
+	private static final String SELECT_KEY_RETURNED_NO_DATA = "SelectKey returned no data.";
 	@Autowired
 	PromiseDao dao;
 
 	@Override
 	public void create(PromiseDto promise) {
-		dao.create(promise);
-		//TODO 발생할 수 있는 에러 -> GroupId가 없음, 약속 날짜가 현재보다 이전, 약속 이름이 null임
+		if (promise.getPromiseDate().getTime() < System.currentTimeMillis()) {
+			throw new InvalidValueException("Promise date must be later than current date.");
+		}
+		if (promise.getTitle() == null || promise.getTitle().equals("")) {
+			throw new InvalidValueException("Promise name cannot be null");
+		}
+		try {
+			dao.create(promise);
+		} catch (DataAccessException e) {
+			if (e.getMessage().contains(SELECT_KEY_RETURNED_NO_DATA)) { //Group이 없을 때 발생
+				throw new GroupNotFoundException(promise.getGroupId());
+			}
+			throw e;
+		}
 	}
 
 	@Override
@@ -26,11 +43,21 @@ public class PromiseServiceImpl implements PromiseService {
 
 	@Override
 	public void update(PromiseDto promise) {
-		dao.update(promise);
+		if (promise.getPromiseDate().getTime() < System.currentTimeMillis()) {
+			throw new InvalidValueException("Promise date must be later than current date.");
+		}
+		if (promise.getTitle() == null || promise.getTitle().equals("")) {
+			throw new InvalidValueException("Promise name cannot be null");
+		}
+		if (dao.update(promise) < 1) {
+			throw new PromiseNotFoundException(promise.getGroupId(), promise.getPromiseId());
+		}
 	}
 
 	@Override
 	public void delete(int groupId, int promiseId) {
-		dao.delete(groupId, promiseId);
+		if (dao.delete(groupId, promiseId) < 1) {
+			throw new PromiseNotFoundException(groupId, promiseId);
+		}
 	}
 }
