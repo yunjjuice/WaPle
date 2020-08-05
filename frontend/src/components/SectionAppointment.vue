2d55eb4b9d4bcd680848f9c475422dac889582b5
최윤주 <yeomju311@naver.com>
2020-08-06 02:34:28 +0900

    [feature] S03P13A204-69 : 투표 장소 좋아요, 취소하기
    약속 내에 있는  장소에 투표하기 기능
    투표 취소하기 기능

--- frontend/src/components/SectionAppointment.vue
+++ frontend/src/components/SectionAppointment.vue
@@ -27,14 +27,16 @@
                   TODO : 투표하기
                   투표 후에 items를 다시 싹 업데이트 해야 voted가 잘 넘어올 텐데 ..?
                 -->
-                <v-btn icon>
-                  <template v-if="!item.voted">
+                <template v-if="!item.voted">
+                  <v-btn icon @click="voteTo(item)">
                     <v-icon>mdi-thumb-up-outline</v-icon>
-                  </template>
-                  <template v-else>
-                    <v-icon>mdi-thumbup</v-icon>
-                  </template>
-                </v-btn>
+                  </v-btn>
+                </template>
+                <template v-else>
+                  <v-btn icon @click="voteCancel(item)">
+                    <v-icon>mdi-thumb-up</v-icon>
+                  </v-btn>
+                </template>
                 {{ item.voteNum }}
               </v-card-text>
             </div>
@@ -59,17 +61,53 @@ export default {
     items: () => store.getters.items,
   },
   created() {
-    api.get(`/votes/${this.appointment.groupId}/${this.appointment.promiseId}`, {
-      headers: {
-        token: this.$session.get('token'),
-      },
-    }).then(({ data }) => {
-      store.dispatch('doUpdate', data);
-    });
+    this.getVotePlaceList();
   },
   methods: {
     moveBack() {
       this.$router.go(-1);
+    },
+    getVotePlaceList() {
+      api.get(`/votes/${this.appointment.groupId}/${this.appointment.promiseId}`, {
+        headers: {
+          token: this.$session.get('token'),
+        },
+      }).then(({ data }) => {
+        store.dispatch('doUpdate', data);
+      });
+    },
+    voteTo(item) { // 해당 장소에 투표
+      api.post('/votes/to', {
+        groupId: store.getters.appointment.groupId,
+        placeId: item.placeId,
+        promiseId: store.getters.appointment.promiseId,
+        userId: this.$session.get('uid'),
+      },
+      {
+        headers: {
+          token: this.$session.get('token'),
+        },
+      }).then((res) => {
+        if (res.status === 201) {
+          this.snackbar = true;
+          this.getVotePlaceList();
+        }
+      });
+    },
+    voteCancel(item) { // 투표 취소
+      const { groupId } = store.getters.appointment;
+      const { promiseId } = store.getters.appointment;
+      const { placeId } = item;
+      const userId = this.$session.get('uid');
+      api.delete(`/votes/to/${groupId}/${promiseId}/${placeId}/${userId}`, {
+        headers: {
+          token: this.$session.get('token'),
+        },
+      }).then((res) => {
+        if (res.status === 204) {
+          this.getVotePlaceList();
+        }
+      });
     },
   },
 };
