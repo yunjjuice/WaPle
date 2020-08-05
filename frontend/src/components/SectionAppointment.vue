3251ccf1149a4fe6a48021375733ed33e9f96038
최윤주 <yeomju311@naver.com>
2020-08-05 20:55:29 +0900

    [feature] S03P13A204-28 : 약속 목록, 투표장소 조회
    1. 약속 목록 조회
    2. 약속 내 장소 조회

--- frontend/src/components/SectionAppointment.vue
+++ frontend/src/components/SectionAppointment.vue
@@ -4,17 +4,68 @@
       <v-btn icon class="hidden-xs-only" @click="moveBack">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <v-toolbar-title>{{ id }}</v-toolbar-title>
+      <v-toolbar-title>{{ appointment.title }}</v-toolbar-title>
     </v-toolbar>
+    <v-row align='center' justify='center'>
+      <v-col
+        v-for="(item, i) in items"
+          :key="i"
+          cols="12"
+      >
+        <v-card
+        >
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title
+                class="headline"
+                v-text="item.name"
+              />
+              <v-card-text>
+                {{ item.address }} <br>
+                {{ item.pickedUserName }}<br>
+                <!--
+                  TODO : 투표하기
+                  투표 후에 items를 다시 싹 업데이트 해야 voted가 잘 넘어올 텐데 ..?
+                -->
+                <v-btn icon>
+                  <template v-if="!item.voted">
+                    <v-icon>mdi-thumb-up-outline</v-icon>
+                  </template>
+                  <template v-else>
+                    <v-icon>mdi-thumbup</v-icon>
+                  </template>
+                </v-btn>
+                {{ item.voteNum }}
+              </v-card-text>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-main>
 </template>
 
 <script>
+import store from '@/store/index';
+import api from '@/utils/api';
+
 export default {
   data() {
     return {
-      id: this.$route.params.id,
     };
+  },
+  computed: {
+    appointment: () => store.getters.appointment,
+    items: () => store.getters.items,
+  },
+  created() {
+    api.get(`/votes/${this.appointment.groupId}/${this.appointment.promiseId}`, {
+      headers: {
+        token: this.$session.get('token'),
+      },
+    }).then(({ data }) => {
+      store.dispatch('doUpdate', data);
+    });
   },
   methods: {
     moveBack() {
