0d8f22ffe13b69af7c7ea07470b5f8569101964c
최윤주 <yeomju311@naver.com>
2020-08-19 14:01:46 +0900

    [etc] S03P13A204-117 : 초대페이지

--- frontend/src/components/Invite.vue
+++ frontend/src/components/Invite.vue
@@ -1,9 +1,25 @@
 <template>
 <v-main>
-  {{ groupName }} 그룹에 초대받았습니다.
-  <v-btn @click="invite">
-    초대 수락하기
-  </v-btn>
+  <v-container fill-height align="center" justify="center">
+    <v-flex row wrap align="center" justify="center">
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="12" align="center" justify="center">
+          <span style="font-family: 'GmarketSansBold'"
+            :style="{'font-size': size = $vuetify.breakpoint.mdAndUp ? '5vh' : '4vh'}"
+          >
+            {{ groupName }}<br>
+            그룹과 함께<br>
+            와플을 만들어주세요<br>
+          </span>
+        </v-col>
+        <v-col cols="12" sm="8" md="12" align="center" justify="center">
+          <button @click="invite" class="skewBtn lorange">
+            토핑 고르러 가기
+          </button>
+        </v-col>
+      </v-row>
+    </v-flex>
+  </v-container>
 </v-main>
 </template>
 
@@ -39,6 +55,47 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+$l_orange : #FBC02D;
+
+@mixin buttonBG($bg) {
+  border: 2px solid $bg;
+  color: $bg;
+  &:hover {
+    background-color: $bg;
+    transition: all 0.3s ease-in-out;
+  }
+}
+
+.container{
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+  height: 97vh;
+  .skewBtn{
+    width: 180px;
+    height: 70px;
+    outline: none;
+    cursor: pointer;
+    background: none;
+    transition: .5s;
+    font-size: 20px;
+    border-radius: 5px;
+    font-family: "GmarketSansMedium",sans-serif;
+    &:hover{
+      border: none;
+      color: white;
+      font-size: 24px;
+      transform:scale(1.1);
+    }
+  }
+
+}
+
+/* Buttons */
+.lorange{
+  @include buttonBG($l_orange);
+}
 
 </style>
